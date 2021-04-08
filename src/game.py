fdac39690caf1b034d28e835997c270b7bf6f4d3
Allen_K <allenkelly278@gmail.com>
2021-04-08 15:40:59 +0100

    pressed for time. moving on with mvp

--- src/game.py
+++ src/game.py
@@ -1,16 +1,20 @@
 from src.player import Player
 
 def game(player1, player2):
+    # create list of valid gestures
+    gestures = ["rock", "paper", "scissors"]
+
     # check input type
-    if type(player1) != Player:
+    if type(player1) != Player or type(player2) != Player:
         return "please enter valid player"
     # check gesture valid
+    if player1.gesture not in gestures or player2.gesture not in gestures:
+        return "play by the rules, a-hole" 
+
+    #if player1.gesture == player2.gesture:
     # if player1 is rock & player2 is scissors
         #return player1 winner
     # if player1 is rock & player2 is paper
         #return player2 winner
     # if player1 is rock & player2 is rock
         #return draw
-
-
-    pass
--- tests/test_game.py
+++ tests/test_game.py
@@ -6,9 +6,19 @@ class TestGame(unittest.TestCase):
     def setUp(self):
         self.player1 = Player("Josh", "rock")
         self.player2 = Player("Spencer", "scissors")
-        self.player = Player("Allen", "paper")
+        self.player3 = Player("Allen", "paper")
+        self.player4 = Player("Cheat", "nunchucks")
+        self.player5 = Player("Colin", "rock")
 
     def test_input_is_valid(self):
         self.assertEqual("please enter valid player", game(1, 2))
 
+    def test_input_1_is_invalid(self):
+        self.assertEqual("please enter valid player", game(self.player1, 2))
+
+    def test_player_has_correct_gesture(self):
+        self.assertEqual("play by the rules, a-hole", game(self.player1, self.player4))
+
+    def test_game_is_draw(self):
+        self.assertEqual("draw", game(self.player1, self.player5))
     
