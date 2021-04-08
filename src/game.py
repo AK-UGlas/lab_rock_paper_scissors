f66262d58259c35ad6a006d83a0881f3858d00c7
Allen_K <allenkelly278@gmail.com>
2021-04-08 16:04:44 +0100

    all tests passing - possible MVP

--- src/game.py
+++ src/game.py
@@ -11,10 +11,27 @@ def game(player1, player2):
     if player1.gesture not in gestures or player2.gesture not in gestures:
         return "play by the rules, a-hole" 
 
-    #if player1.gesture == player2.gesture:
+    # if gestures are equal its a draw
+    if player1.gesture == player2.gesture:
+        return "draw"
+    
     # if player1 is rock & player2 is scissors
         #return player1 winner
+    if player1.gesture == "rock":
+        if player2.gesture == "scissors":
+            return "rock beats scissors"
+        return "paper beats rock"
     # if player1 is rock & player2 is paper
         #return player2 winner
-    # if player1 is rock & player2 is rock
-        #return draw
+    elif player1.gesture == "scissors":
+        if player2.gesture == "paper":
+            return "scissors beats paper"
+        return "rock beats scissors"
+
+    # play
+    elif player1.gesture == "paper":
+        if player2.gesture == "rock":
+            return "paper beats rock"
+        return "scissors beats paper"
+
+    
--- tests/test_game.py
+++ tests/test_game.py
@@ -21,4 +21,23 @@ class TestGame(unittest.TestCase):
 
     def test_game_is_draw(self):
         self.assertEqual("draw", game(self.player1, self.player5))
+
+    def test_rock_beats_scissors(self):
+        self.assertEqual("rock beats scissors", game(self.player1, self.player2))
+    
+    def test_paper_beats_rock(self):
+        self.assertEqual("paper beats rock", game(self.player1, self.player3))
+
+    def test_scissors_beats_paper(self):
+        self.assertEqual("scissors beats paper", game(self.player2, self.player3))
+    
+    def test_rock_beats_scissors_player2(self):
+        self.assertEqual("rock beats scissors", game(self.player2, self.player1))
+
+    def test_paper_beats_rock_player1(self):
+        self.assertEqual("paper beats rock", game(self.player3, self.player1))
+
+    def test_scissors_beats_paper_player1(self):
+        self.assertEqual("scissors beats paper", game(self.player3, self.player2))
+
     
