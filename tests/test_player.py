667aec0ef4676e5516a6f073700d491114abd39d
Allen_K <allenkelly278@gmail.com>
2021-04-08 14:54:23 +0100

    test Player class passing

--- tests/test_player.py
+++ tests/test_player.py
@@ -2,4 +2,12 @@ import unittest
 from src.player import Player
 
 class TestPlayer(unittest.TestCase):
-    pass
+    def setUp(self):
+        self.player = Player("Allen", "paper")
+
+    def test_player_has_name(self):
+        self.assertEqual("Allen", self.player.name)
+
+    def test_player_has_gesture(self):
+        self.assertEqual("paper", self.player.gesture)
+    
